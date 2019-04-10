4a973e5ff930ba7c0b660a4958bc82a55ba7186d
Md. Moin Khan Robin <IamRJRobin@hotmail.com>
2019-04-10 21:14:29 +0600

    commitImageDisplayOk

---
+++ eCommerce/eCommerce/AllProducts.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AllProducts.aspx.cs" Inherits="eCommerce.AllProducts" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+          
+                   <asp:GridView ID="allProductTable" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="allProductTable_SelectedIndexChanged">
+            <Columns>
+            <asp:BoundField DataField="ProductID" HeaderText="ID" />
+            
+                
+            <asp:TemplateField HeaderText="Image">
+            <ItemTemplate>
+                <asp:Image ID="Image1" runat="server" Height="100px" Width="300px"
+                    ImageUrl='<%# "data:Image/png;base64,"
+                    + Convert.ToBase64String((byte[])Eval("ProductPhoto")) %>' />
+            </ItemTemplate>
+             </asp:TemplateField>
+
+            
+            <asp:BoundField DataField="ProductName" HeaderText="Name" />
+            <asp:BoundField DataField="ProductCat" HeaderText="Category" />
+            <asp:BoundField DataField="ProductPrice" HeaderText="Price" />
+            
+            </Columns>
+            </asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
--- eCommerce/eCommerce/ManagerAddProduct.aspx.cs
+++ eCommerce/eCommerce/ManagerAddProduct.aspx.cs
@@ -32,30 +32,68 @@ namespace eCommerce
             HttpPostedFile postedFile=photoUpload1.PostedFile;
             string fileName = Path.GetFileName(postedFile.FileName);
             string fileExtension = Path.GetExtension(fileName);
-            int length = photoUpload1.PostedFile.ContentLength; ;
+            int fileSize = postedFile.ContentLength;
+            // int length = photoUpload1.PostedFile.ContentLength; ;
             if(fileExtension.ToLower()==".jpg"|| fileExtension.ToLower() == ".bpm"|| fileExtension.ToLower() == ".png" )
             {
-                // Stream strm = postedFile.InputStream;
-                // BinaryReader bReader = new BinaryReader(strm);
-               // byte[] picture = new byte(length);
-                    
-                 DataOps dOps = new DataOps();
-                //string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-                // SqlConnection conn = new SqlConnection(connectionString);
+                 Stream strm = postedFile.InputStream;
+                 BinaryReader bReader = new BinaryReader(strm);
+                // byte[] picture = new byte(length);
+                byte[] bytes=bReader.ReadBytes((int)strm.Length);
+                string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spUploadImage",con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlParameter pName = new SqlParameter()
+                    {
+                        ParameterName = "@ProductName",
+                        Value = txtBoxProductName.Text
+                    };
+                    cmd.Parameters.Add(pName);
 
-                // SqlCommand cmd = new SqlCommand("addProduct", conn);
-                // cmd.CommandType = CommandType.StoredProcedure;
-                string query = @"INSERT INTO [dbo].[ProductTable]
-                          ([ProductName]
-                          ,[ProductCat]
-                          ,[ProductPrice]
-                          ,[ProductPhoto])
-                    VALUES('"+txtBoxProductName.Text+ "','" + txtBoxProductCategory.Text + "','" + txtBoxProductPrice.Text + "','" + photoUpload1.FileName+ "')";
+                    SqlParameter pCat = new SqlParameter()
+                    {
+                        ParameterName = "@ProductCat",
+                        Value = txtBoxProductCategory.Text
+                    };
+                    cmd.Parameters.Add(pCat);
+
+                    SqlParameter pPrice = new SqlParameter()
+                    {
+                        ParameterName = "@ProductPrice",
+                        Value = txtBoxProductPrice.Text
+                    };
+                    cmd.Parameters.Add(pPrice);
+
+                    SqlParameter pPhoto = new SqlParameter()
+                    {
+                        ParameterName = "@ProductPhoto",
+                        Value = bytes
+                    };
+                    cmd.Parameters.Add(pPhoto);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+
+                    // DataOps dOps = new DataOps();
+                    //string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                    // SqlConnection conn = new SqlConnection(connectionString);
+
+                    // SqlCommand cmd = new SqlCommand("addProduct", conn);
+                    // cmd.CommandType = CommandType.StoredProcedure;
+                    //               string query = @"INSERT INTO [dbo].[ProductTable]
+                    //                         ([ProductName]
+                    //                         ,[ProductCat]
+                    //                        ,[ProductPrice]
+                    //                        ,[ProductPhoto])
+                    //                 VALUES('"+txtBoxProductName.Text+ "','" + txtBoxProductCategory.Text + "','" + txtBoxProductPrice.Text + "','" + photoUpload1.FileName+ "')";
 
 
-                dOps.ManagerAddProduct(query);
-                
-                lblMessage.Visible = true;
+                    //              dOps.ManagerAddProduct(query);
+
+                    lblMessage.Visible = true;
                 lblMessage.Text = "Added!";
                 lblMessage.ForeColor = System.Drawing.Color.Green;
             }
